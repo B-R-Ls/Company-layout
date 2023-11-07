64c1fb8f16c6da0a220e7af05888cb0741fd2de5
Brendan <brendan.r.le@gmail.com>
2023-11-07 00:35:19 -0600

    added functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,26 @@
 INSERT INTO department (name)
-VALUES (""),
+VALUES ("Engineering"),
+       ("Cyber Development"),
+       ("Marketing"),
+       ("Sales")
 
-INSERT INTO role (title, salary)
-VALUES (""),
 
-INSERT INTO employee (first_name, last_name)
-VALUES (""),
+INSERT INTO role (title, salary, department_id)
+VALUES ("Cheif_Engineer", 80,000, 1),
+       ("Engineer", 60,000, 1),
+       ("Cyber_Cheif", 80,000, 2),
+       ("Cyber_engineer", 60,000, 2),
+       ("Marketing_Cheif", 70,000, 3),
+       ("Marketing_Agent", 50,000, 3),
+       ("Sales_Mangager", 65,000, 4),
+       ("Sales_Person", 40,000, 4)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ben", "Franco", 1, null),
+       ("Tom", "markus", 2, 1),
+       ("Deep", "Miriam", 3, null),
+       ("Kamen", "Alfons", 4, 3),
+       ("Tina", "Julia", 5, null),
+       ("Susanna", "Keaton", 6, 5),
+       ("Catherin", "Hasan", 7, null),
+       ("Samuel", "Girish", 8, 7)
--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const db = mysql.createConnection(
     console.log(`Connected to the courses_db database.`)
 );
 
-var roles = []
+var roles = ['Cheif_Engineer', 'Engineer', 'Cyber_Cheif', 'Cyber_Engineer', 'Marketing_Cheif', 'Marketing_Agent', 'Sales_Manager', 'Sales_Person']
+var managers = ['none','Ben', 'Deep', 'Tina', 'Catherin']
+var departments = ['Engineering', 'Cyber Development', 'Marketing', 'Sales']
 
 function init () {
     console.log(
@@ -38,7 +40,10 @@ function init () {
         })
         .render()
     );
+    questions();
+}
 
+function questions() {
     inquirer
         .prompt([
             {
@@ -51,7 +56,7 @@ function init () {
                     'view all employees',
                     'add a department',
                     'add a role',
-                    'add an emplyee',
+                    'add an employee',
                     'update an employee role'
                 ]
             }
@@ -59,17 +64,22 @@ function init () {
         .then((data) => {
             switch (data.initchoice) {
                 case 'view all departments':
-                    db.query('SELECT * FROM department', function (err, table) {
-                        console.table(table)
+                    db.query('SELECT name FROM department', function (err, table) {
+                        console.table(table);
                     });
                     break;
                 case 'view all roles':
-                    db.query('SELECT * FROM role', function (err, table) {
+                    db.query('SELECT title, salary FROM role', function (err, table) {
                         console.table(table)
                     });
                     break;
                 case 'view all employees':
-                    db.query('SELECT * FROM employee', function (err, table) {
+                    db.query(`SELECT 
+                    department.name, role.title, role.salary, employee.first_name, employee.last_name, manager.id as manager
+                    FROM employee 
+                    inner JOIN role ON role.id = employee.role_id 
+                    inner JOIN department ON department.id = role.department_id
+                    left JOIN employee as manager ON employee.manager_id = manager.id;`, function (err, table) {
                         console.table(table)
                     });
                     break;
@@ -82,9 +92,12 @@ function init () {
                                 name: 'dname',
                             }
                         ]) .then((data) => {
+                            departments.push(`${data.dname}`);
                             db.query(`INSERT INTO department (name) VALUES('${data.dname}')`, function (err, table) {
                             console.table(table)
                             });
+                            console.log('test')
+                            
                         })
                     break;
                 case 'add a role':
@@ -99,12 +112,32 @@ function init () {
                             type:'input',
                             message:'How much does this role pay?',
                             name:'pay',
+                        },
+                        {
+                            type:'rawlist',
+                            message:'Which Department is it part of?',
+                            name:'dname',
+                            choices: departments
                         }
-                    ]) .then((data) => {
-                        roles.push(`${data.rname}`);
-                        db.query(`INSERT INTO role (title, salary) VALUES('${data.rname}','${data.pay}')`, function (err, table) {
-                        console.table(table);
-                        });
+                    ])
+                     .then((data) => {
+                        var choice = 0
+
+                        if (data.dname === 'Engineering') {
+                            var choice = 1
+                        } else if (data.dname === 'Cyber Development') {
+                            var choice = 2
+                        } else if (data.dname === 'Marketing') {
+                            var choice = 3
+                        } else {
+                            var choice = 4
+                        }
+                        
+                        db.query(`INSERT INTO role (title, salary, department_id) VALUES('${data.rname}',${data.pay}, ${choice})`, function (err, table) {
+                        db.query(`SELECT title, salary FROM role`, function (err, data) {
+                            console.table(data);
+                        })
+                       }) 
                     })
                     break;
                 case 'add an employee':
@@ -112,26 +145,132 @@ function init () {
                         .prompt([
                             {
                                 type:'input',
-                                message:'What is the name of the employee?',
-                                name: 'ename',
+                                message:'What is the first name of the employee?',
+                                name: 'fename'
+                            },
+                            {
+                                type:'input',
+                                message:'Whate is the last name of the employee?',
+                                name: 'lename'
                             },
                             {
                                 type:'list',
-                                message:'What is his role',
+                                message:'What is their role',
                                 name: 'rchoice',
                                 choices: roles
+                            },
+                            {
+                                type:'list',
+                                message:`Who's their manager?`,
+                                name:'manager',
+                                choices: managers
                             }
                         ]) .then((data) => {
-                            db.query(`INSERT INTO department (name) VALUES('${data.ename}')`, function (err, table) {
-                            console.table(table)
+                            var choice = 0;
+                            var role = 0;
+
+                            if (data.rchoice === 'Cheif_Engineer') {
+                                var role = 1;
+                            } else if (data.rchoice === 'Engineer') {
+                                var role =2;
+                            } else if (data.rchoice === 'Cyber_Cheif') {
+                                var role =3;
+                            } else if (data.rchoice === 'Cyber_Engineer') {
+                                var role =4;
+                            } else if (data.rchoice === 'Marketing_Cheif') {
+                                var role =5;
+                            } else if (data.rchoice === 'Marketing_Agent') {
+                                var role =6;
+                            } else if (data.rchoice === 'Sales_Manager') {
+                                var role =7;
+                            } else  {
+                                var role =8;
+                            } 
+
+                            if (data.manager === 'Ben') {
+                                var choice = 1;
+                            } else if (data.manager === 'Deep') {
+                                var choice = 3;
+                            } else if (data.manager === 'Tina') {
+                                var choice = 5;
+                            } else if (data.manger === 'Catherin') {
+                                var choice = 7;
+                            } else {
+                                var choice = null;
+                            }
+                            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES('${data.fename}','${data.lename}', ${role} ,${choice})`, function (err, table) {
+                            console.log(table)
+                            });
+                            db.query(`SELECT 
+                                department.name, role.title, role.salary, employee.first_name, employee.last_name, manager.id as manager
+                               FROM employee 
+                               inner JOIN role ON role.id = employee.role_id 
+                               inner JOIN department ON department.id = role.department_id
+                               left JOIN employee as manager ON employee.manager_id = manager.id;`, function (err, info) {
+                                    console.table(info);
                             });
                         })
                     break;
                 case 'update an employee role':
-
+                    inquirer
+                        .prompt([
+                            {
+                                type:'input',
+                                message:'What is the first name of the employee?',
+                                name: 'fename'
+                            },
+                            {
+                                type:'list',
+                                message:'What is their new role',
+                                name: 'rchoice',
+                                choices: roles
+                            },
+                        ]) .then((data) => {
+                            var choice = 0;
+                            
+                            if (data.rchoice === 'Cheif_Engineer') {
+                                var choice = 1;
+                            } else if (data.rchoice === 'Engineer') {
+                                var choice =2;
+                            } else if (data.rchoice === 'Cyber_Cheif') {
+                                var choice =3;
+                            } else if (data.rchoice === 'Cyber_Engineer') {
+                                var choice =4;
+                            } else if (data.rchoice === 'Marketing_Cheif') {
+                                var choice =5;
+                            } else if (data.rchoice === 'Marketing_Agent') {
+                                var choice =6;
+                            } else if (data.rchoice === 'Sales_Manager') {
+                                var choice =7;
+                            } else  {
+                                var choice =8;
+                            } 
+                            db.query(`UPDATE employee SET role_id = ${choice} WHERE first_name = '${data.fename}'`, function (err, table) {
+                            console.table(table)
+                            }) 
+                            db.query(`SELECT 
+                                department.name, role.title, role.salary, employee.first_name, employee.last_name, manager.id as manager
+                               FROM employee 
+                               inner JOIN role ON role.id = employee.role_id 
+                               inner JOIN department ON department.id = role.department_id
+                               left JOIN employee as manager ON employee.manager_id = manager.id;`, function (err, table) {
+                               console.table(table);
+                            })
+                            
+                        })
                     break;
             }
+            return;
         })
+        return;
 }
 
 init();
+
+// db.query('SELECT name FROM department', function (err, table) {
+//     for (let i = 0; i < table.length; i++) {
+        // console.log(Object.values(table[i]))
+//         const department = Object.values(table[i]);
+//         return departments
+//     }
+// }) 
